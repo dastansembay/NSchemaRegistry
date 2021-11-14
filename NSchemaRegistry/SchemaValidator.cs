816a785f10751e25b80a0d32f97d807338de21e6
dastansembay <dastan.sembay@netmoregroup.com>
2021-11-15 00:30:01 +0600

    Add project files.

---
+++ NSchemaRegistry/SchemaValidator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NJsonSchema;
+
+namespace NSchemaRegistry
+{
+    public static class SchemaValidator
+    {
+        private const string SchemasFolder = "Schemas";
+
+        public static async Task<SchemaValidationResult> Validate(string data, string type, int version = 1)
+        {
+            var schema = await LoadSchema(type, version);
+            var validationErrors = schema.Validate(data);
+
+            if (validationErrors.Any())
+            {
+                return new SchemaValidationResult
+                {
+                    Success = false, 
+                    Errors = validationErrors.Select(t => new SchemaValidationError(t.Property, t.Kind.ToString("G"))).ToList()
+                };
+            }
+
+            return new SchemaValidationResult {Success = true};
+        }
+
+        private static async Task<JsonSchema> LoadSchema(string type, int version)
+        {
+            var delimiter = type.IndexOfAny(new[] { '.', '_' });
+            var domain = type[..delimiter];
+            var schemaTypeName = type[(delimiter + 1)..];
+
+            var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), SchemasFolder);
+            path = Path.Combine(path, domain);
+            path = Path.Combine(path, schemaTypeName);
+            path = Path.Combine(path, $"{version}.json");
+            string jsonSchema;
+            try
+            {
+                jsonSchema = File.ReadAllText(path);
+            }
+            catch (Exception e)
+            {
+                throw new SchemaLoadException($"Error reading schema {type} v{version}", e);
+            }
+            
+            return await JsonSchema.FromJsonAsync(jsonSchema);
+        }
+    }
+
+    public class SchemaLoadException : Exception
+    {
+        public override string Message { get; }
+        public SchemaLoadException(string message)
+        {
+            Message = message;
+        }
+        public SchemaLoadException(string message, Exception e) : base(message, e) { }
+    }
+
+    public class SchemaValidationResult
+    {
+        public bool Success { get; set; }
+        public IReadOnlyList<SchemaValidationError> Errors { get; set; } = new List<SchemaValidationError>(0);
+    }
+
+    public class SchemaValidationError
+    {
+        public SchemaValidationError(string property, string error)
+        {
+            Property = property;
+            Error = error;
+        }
+
+        public string Property { get; set; }
+        public string Error { get; set; }
+    }
+}
---
+++ SchemaValidationExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using NSchemaRegistry;
+
+namespace SchemaValidationExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var payload = new
+            {
+                event_id = Guid.NewGuid(),
+                event_name = "AccountChanged",
+                event_version = 1,
+                data = new
+                {
+                    PublicId = Guid.NewGuid(),
+                    Username = "Johndoe",
+                    Role = "user"
+                }
+            };
+            
+            var validationResult = await SchemaValidator.Validate(JsonSerializer.Serialize(payload, new JsonSerializerOptions {PropertyNameCaseInsensitive = true}), "Accounts.AccountChanged", 1);
+            Console.WriteLine($"Validation result: {validationResult.Success};");
+
+
+            var notValidPayload = new
+            {
+                event_id = Guid.NewGuid(),
+                event_name = "AccountChanged",
+                event_version = 1,
+                data = new
+                {
+                    PublicId = Guid.NewGuid(),
+                    Username = "Johndoe",
+                    Role = 1
+                }
+            };
+
+            var validationResult2 = await SchemaValidator.Validate(JsonSerializer.Serialize(notValidPayload), "Accounts.AccountChanged", 1);
+            Console.WriteLine($"Validation result: {validationResult2.Success}; errors: [{string.Join(',', validationResult2.Errors.Select(e => $"{e.Property}: {e.Error}"))}]");
+        }
+    }
+}
